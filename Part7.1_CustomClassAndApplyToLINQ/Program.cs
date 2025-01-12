7b86167a33ce209002ab8575d76ef21cceae1c99
JN <joyng808@gmail.com>
2025-01-12 16:11:34 +0800

    [JN] - Added and Update Lamda Functionality not exist on LINQ

--- Part7.1_CustomClassAndApplyToLINQ/Program.cs
+++ Part7.1_CustomClassAndApplyToLINQ/Program.cs
@@ -177,107 +177,6 @@ namespace App.Fruits
             #endregion -- END Grouping for LINQ and LAMBDA --
 
 
-
-            // --------------------------------------------------------------------
-            // ------- AGREGATION LAMBDA ONLY Means not Available in LINQ --------
-            // --------------------------------------------------------------------
-            #region  -- AGREGATION LAMBDA ONLY Means not Available in LINQ  --
-            Console.WriteLine("*********************");
-            Console.WriteLine (" -- AGREGATION THAT AVAILABLE IN LAMNDA   -- ");
-            Console.WriteLine ();
-
-            var numbers = Enumerable.Range(1, 10); //equivalent to {1,2,3,4,5,6,7,8,9,10} //shortcut to make 1 - 10 
-            Console.WriteLine(string.Join(",", numbers )); //join the numbers with seperator of , 
-
-            //Average 
-            Console.WriteLine ();
-            Console.WriteLine (" -- average  num1To5Average  -- ");
-            var average = numbers.Average();
-            var num1To5Average = numbers.Where (n => n <= 5 ).Average();
-            Console.WriteLine($"Average: {average}, Average 1 to 5: {num1To5Average}");
-
-            //Count or long Count, use Long Count when expected count is more that int capacity 
-            var count = numbers.Count();
-
-            //Max and Mix 
-            var maxNumber = numbers.Max();
-            var minNumber = numbers.Min();
-
-            //Sum 
-            var summation = numbers.Sum();
-
-            Console.WriteLine ();
-            Console.WriteLine (" -- Count  Max  MIN  SUM  -- ");
-            Console.WriteLine($"Count:{count}, Max: {maxNumber}, Min: {minNumber}, Sum: {summation}");
-
-
-            #endregion --LAMNDA ONLY
-
-
-            // --------------------------------------------------------------------
-            // ------- CONVERTION LAMBDA ONLY Means not Available in LINQ --------
-            // --------------------------------------------------------------------
-            #region  -- CONVERTION LAMBDA ONLY Means not Available in LINQ  --
-            Console.WriteLine("*********************");
-            Console.WriteLine (" -- CONVERTION THAT AVAILABLE IN LAMNDA   -- ");
-            Console.WriteLine ();
-
-            var numberstoConvert = Enumerable.Range(1, 10); //equivalent to {1,2,3,4,5,6,7,8,9,10} //shortcut to make 1 - 10 
-            Console.WriteLine(string.Join(",", numberstoConvert )); //join the numbers with seperator of ,
-
-            var list = new List<int> (numberstoConvert); // dito naka list ab int tayo tas yung sa line 232 is naka IEnumerable <int> so to fixxed the error need natin iconvert to list 
-            var enumValue = list.AsEnumerable(); //converted to enumerable 
-            var backToList = enumValue.ToList(); //converted back to list 
-
-            //list = (from number in numberstoConvert where number >= 5 select number); //as per discussion yung LINQ and LAMBDA is always IEnumerable line IEnumerable<int> , IEnumerable <string> , IEnumerable <strongly type > and so on 
-
-            //Convert IEnumerable <int> to List 
-            list = (from number in numberstoConvert where number >= 5 select number).ToList();
-
-            Console.WriteLine ();
-            #endregion 
-
-
-            // ------------------------------------------------------------------------------------
-            // ------- BASIC ELEMENT ACCESS FUNCTION OF  LAMBDA ONLY Means not Available in LINQ --------
-            // ------------------------------------------------------------------------------------
-            #region  --BASIC ELEMENT ACCESS FUNCTION OF  LAMBDA ONLY Means not Available in LINQ  --
-            Console.WriteLine("*********************");
-            Console.WriteLine (" -- BASIC ELEMENT ACCESS FUNCTION THAT AVAILABLE IN LAMNDA   -- ");
-            Console.WriteLine ();
-
-            var numbersElemet = Enumerable.Range(1, 10); // equivalent to {1,2,3,4,5,6,7,8,9,10}
-            Console.WriteLine(string.Join(",", numbersElemet)); // join the numbers with separator of ,
-
-            var firstNumber = numbersElemet.FirstOrDefault();
-            Console.WriteLine("First Number (with default): " + firstNumber);
-
-            var lastNumber = numbersElemet.LastOrDefault();
-            Console.WriteLine("Last Number (with default): " + lastNumber);
-
-            var firstEvenNumber = numbersElemet.FirstOrDefault(n => n % 2 == 0);
-            Console.WriteLine("First Even Number: " + firstEvenNumber);
-
-            var firstWithoutDefault = numbersElemet.First();
-            Console.WriteLine("First Number (without default): " + firstWithoutDefault);
-
-            var lastWithoutDefault = numbersElemet.Last();
-            Console.WriteLine("Last Number (without default): " + lastWithoutDefault);
-
-            Console.WriteLine ();
-            #endregion
-
-
-            // ------------------------------------------------------------------------------------
-            // ------- PARTITIONING OF LAMBDA ONLY Means not Available in LINQ --------
-            // ------------------------------------------------------------------------------------
-            #region  -- PARTITIONING OF LAMBDA ONLY Means not Available in LINQ  --
-            Console.WriteLine("*********************");
-            Console.WriteLine (" -- PARTITIONING OF   -- ");
-            Console.WriteLine ();
-
-            #endregion
-
             // -------------------------------
             // ------- Other Samples --------
             // -------------------------------
@@ -305,9 +204,6 @@ namespace App.Fruits
                 Console.WriteLine($"Name: {student.Name}, Age: {student.Age}, Grade: {student.Grade}");
             }
             #endregion --other sample end --
-
-
-
 
 
             #endregion
--- obj/Debug/net8.0/app.AssemblyInfo.cs
+++ obj/Debug/net8.0/app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+296d4acd6f15a6b76412c2a5f0a83d7d791799e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f65ba6a904113176a440b37b2eca64575c2161c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
